e4661e61e59661f559a8c92c52b908c12d1037a6
tobu-git <to.bu@gmx.net>
2026-03-19 14:00:19 +0100

    refactor: Repositioning main function to top in bikeshare
    Main functions moved to top for logical order and thus better readability.
    
     - bikeshare.py changed slightly
    
    Resolves: Submission task 4

--- bikeshare.py
+++ bikeshare.py
@@ -5,6 +5,24 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+def main():
+    while True:
+        city, month, day = get_filters()
+        df = load_data(city, month, day)
+
+        time_stats(df)
+        station_stats(df)
+        trip_duration_stats(df)
+        user_stats(df)
+
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
+
+
+if __name__ == "__main__":
+	main()
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -117,20 +135,4 @@ def user_stats(df):
     print('-'*40)
 
 
-def main():
-    while True:
-        city, month, day = get_filters()
-        df = load_data(city, month, day)
 
-        time_stats(df)
-        station_stats(df)
-        trip_duration_stats(df)
-        user_stats(df)
-
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
-            break
-
-
-if __name__ == "__main__":
-	main()
