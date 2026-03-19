1c04c5d7a2e2bb8c741091b0959a54e2d82d8eb2
tobu-git <to.bu@gmx.net>
2026-03-19 13:59:26 +0100

    refactor: Adding comment to trip_duration_stats in bikeshare
    Improvement to readability by adding comment to trip_duration_stats.
    
     - only bikeshare.py changed
    
    Resolves: Submission task 4

--- bikeshare.py
+++ bikeshare.py
@@ -81,7 +81,7 @@ def station_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-
+# method to calculate trip duration stats
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
 
